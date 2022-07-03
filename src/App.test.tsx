a2f30d8123f75f1a41b92df0255550b89985167a
Rafael Rehn <rafael.rehn@projuris.com.br>
2022-07-03 13:55:30 -0300

    Fix unit test name

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
 describe('App:', ()=>{
-  test('renders learn react link', () => {
+  test('Render react app', () => {
     const { container } = render(<MemoryRouter><App /></MemoryRouter>);
     expect(container.getElementsByClassName("app").length).toBe(1)
   });
